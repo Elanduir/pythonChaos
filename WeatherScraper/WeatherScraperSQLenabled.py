45ac8df83468a84da402161b6370e64085b956df
Fabian Heuberger <elanduir@gmail.com>
2022-06-21 18:58:49 +0200

    added heat index

--- WeatherScraper/WeatherScraperSQLenabled.py
+++ WeatherScraper/WeatherScraperSQLenabled.py
@@ -2,6 +2,7 @@
 import requests
 import mysql.connector
 from datetime import datetime
+from heat_index import calculate
 from bs4 import BeautifulSoup
 import time
 import os
@@ -22,16 +23,13 @@ config = {
 
 db = mysql.connector.connect(**config)
 dbcursor = db.cursor()
-sqlW = "INSERT INTO 5610_wohlen_cache (date_created, temperature, humidity, pressure) VALUES (%s, %s, %s, %s)"
-sqlL = "INSERT INTO 5600_lenzburg_cache (date_created, temperature, humidity, pressure) VALUES (%s, %s, %s, %s)"
+sqlW = "INSERT INTO 5610_wohlen_cache (date_created, temperature, humidity, pressure, heatIndex) VALUES (%s, %s, %s, %s, %s)"
+sqlL = "INSERT INTO 5600_lenzburg_cache (date_created, temperature, humidity, pressure, heatIndex) VALUES (%s, %s, %s, %s, %s)"
 
 while(True):
     fDate = datetime.now().strftime('%Y-%m-%d %H:%M')
     
-    pageW = requests.get(URL_wohlen, proxies=dict(http= 'socks5://proxyaxe.com:9866', https='socks5://proxyaxe.com:9866'))
-
-    print(pageW)
-
+    pageW = requests.get(URL_wohlen)
     pageL = requests.get(URL_lenzburg)
 
     soupW = BeautifulSoup(pageW.content, "html.parser")
@@ -39,24 +37,26 @@ while(True):
 
     resultW = soupW.find_all("td", class_="akt_daten_wert farbe_tab_akt")
     resultL = soupL.find_all("span", class_="Stil3")
-
-    tempW = resultW[1].text
-    humW = resultW[2].text
-    preW = resultW[3].text
-
-    tempL = resultL[0].text.strip()
-    humL = resultL[4].text.strip()
-    preL = resultL[12].text.strip().splitlines()[0]
-
-    valW = (fDate, float(tempW[0:4]), float(humW[0:2]), float(preW[0:6]))
-    valL = (fDate, float(tempL[0:4]), float(humL[0:2]), float(preL[0:6]))
     
+    rHW = soupW.find_all("td", class_="akt_daten_wert farbe_tab_stat")
 
-    print(valL)
+    tempW = resultW[1].text[0:4]
+    humW = resultW[2].text[0:2]
+    preW = resultW[3].text[0:6]
+    hdexW = rHW[1].text[0:4]
 
+    tempL = resultL[0].text.strip()[0:4]
+    humL = resultL[4].text.strip()[0:2]
+    preL = resultL[12].text.strip().splitlines()[0][0:6]
+    hdexL = round(calculate.from_celsius(tempL, humL), 1)
+
+    valW = (fDate, float(tempW), float(humW), float(preW), float(hdexW))
+    valL = (fDate, float(tempL), float(humL), float(preL), hdexL)
+
+    
     dbcursor.execute(sqlW, valW)
     dbcursor.execute(sqlL, valL)
     db.commit()
     print("Wrote data to DB")
-    time.sleep(60)
+    time.sleep(600)
 
