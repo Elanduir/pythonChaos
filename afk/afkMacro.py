c5423b95c1ca440e4b3e01454fd5956aa7b8c377
Fabian Heuberger <elanduir@gmail.com>
2023-03-17 19:17:21 +0100

    added macro for mouse movement

---
+++ afk/afkMacro.py
@@ -0,0 +1,23 @@
+from pynput.keyboard import Key, Controller as KeyboardController
+from pynput.mouse import Button, Controller as MouseController
+from time import sleep
+
+mouse = MouseController()
+keyboard =KeyboardController()
+sleep(5)
+
+while True:
+    mouse.move(-300,-300)
+    sleep(1)
+    mouse.click(Button.left, 1)
+    sleep(1)
+    keyboard.press('1')
+    sleep(0.5)
+    keyboard.release('1')
+    sleep(0.5)
+    mouse.move(300,300)
+    sleep(1)
+    keyboard.press('2')
+    sleep(0.5)
+    keyboard.release('2')
+    sleep(0.5)
