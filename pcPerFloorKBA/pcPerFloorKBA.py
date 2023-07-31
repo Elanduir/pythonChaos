c92ca8357399c9596432da15e3e709e11e185bd2
Elanduir <elanduir@gmail.com>
2023-07-31 13:13:24 +0000

    added auto input for reqUsers added output to csv

--- pcPerFloorKBA/pcPerFloorKBA.py
+++ pcPerFloorKBA/pcPerFloorKBA.py
@@ -1,4 +1,6 @@
 import csv
+import re
+from os import walk
 
 # ComputerData class declaration to store all info
 class ComputerData:
@@ -11,17 +13,47 @@ class ComputerData:
         self.mac1 = ""
         self.mac2 = ""
         self.userList = []
-        self.arztLogin = False
+
+    def convertUserlist(self):
+        uList = []
+        for u in self.userList:
+            uList.append((", ").join(u))
+        return ("\n").join(uList)
+
+    def __iter__(self):
+        return iter([self.name, self.ci, self.location, self.model, self.status, self.mac1, self.mac2, self.convertUserlist()])
+
+   
 
     def __str__(self):
         return str(self.name) + "\n" + str(self.ci) + "\n" + str(self.model) + "\n" + str(self.location) +"\n" + str(self.status) + "\n" + str(self.mac1) + "\n" + str(self.mac2) + "\n" + str(self.userList)
 
-
+migPath = 'resources/migData'
 cleanHost = []
 cleanUser = []
 doc = []
 compLoc = {}
 compDat = {}
+user0802 = []
+user0803 = []
+comp0802 = []
+
+def userFilter(compList, userList):
+    result = [];
+    for comp in compList:
+        for uC in comp.userList:
+            if uC[0] in userList:
+                result.append(comp)
+    return result
+
+def writeCompCSV(fileLoc, compList):
+    with open(fileLoc, "w") as csv_output:
+        wr = csv.writer(csv_output, delimiter= ';')
+        wr.writerow(["Hostname", "CI", "Location", "Model", "Status", "Mac1", "Mac2", "Users"])
+        for comp in compList:
+            wr.writerow(list(comp))
+    
+
 
 with open('resources/Hostnamen.csv') as hostnamenCsv:
     reader = csv.reader(hostnamenCsv, delimiter=',')
@@ -62,11 +94,14 @@ with open('resources/aerzte') as arzt:
     for line in arzt:
         doc.append(line.strip())
 
-
-
 for row in cleanHost:
+    match = re.search("(?<=\()[a-zA-Z0-9]+(?=\))", row[1])
+    if match:
+        user = match.group()
+    else:
+        user = "no user found"
     if row[0] in compDat:
-        compDat[row[0]].userList.append([row[1], row[2]])
+        compDat[row[0]].userList.append([user, row[2]])
     else:
         cacheComp = ComputerData()
         cacheComp.name = row[0]
@@ -77,9 +112,17 @@ for row in cleanHost:
             cacheComp.status = compLoc[cacheComp.name][9]
             cacheComp.mac1 = compLoc[cacheComp.name][15]
             cacheComp.mac2 = compLoc[cacheComp.name][17]
-        cacheComp.userList.append([row[1], row[2]])
+        cacheComp.userList.append([user, row[2]])
         compDat[row[0]] = cacheComp
 
 
-for row in compDat:
-    print(compDat[row])
+for (dirpath, dirnames, filenames) in walk(migPath):
+    for fileN in filenames:
+        filePath = dirpath + "/" + fileN
+        reqUser = []
+        with open(filePath) as inputFile:
+            for line in inputFile:
+                reqUser.append(line.strip())
+        compReq = userFilter(compDat.values(), reqUser)
+        writeCompCSV("output/" + fileN + ".csv", compReq)
+            
