a6936c4ae46868461a5b3df4b27aa1c1a2afbb6c
fabian <elanduir@gmail.com>
2023-08-10 13:04:13 +0200

    removed script

--- RouteDuration/ObjectGenerator/src/script.js
+++ RouteDuration/ObjectGenerator/src/script.js
@@ -2,21 +2,21 @@ let paths = [];
 let missing = [];
 let str = "";
 let lookup = {
-    "Wiesen Davos":"13",
-    "Monstein alt Davos":"12",
-    "Monstein neu Davos":"11",
-    "Glaris Davos":"9",
-    "Frauenkirch Davos":"8",
-    "Sertig Davos":"10",
-    "Pauluskirche Davos":"7",
-    "Marienkirche Davos":"5",
-    "St. Johann Davos":"6",
-    "Alexanderhaus Davos":"14",
-    "Englische Kirche Davos":"4",
-    "Herz Jesu Davos":"3",
-    "St. Theodul Davos":"1",
-    "Tschuggen Davos":"2",
-    "Laret Davos":"0",
+    "7494 Wiesen, Switzerland":"13",
+    "Monstein, 7278 Davos, Switzerland":"11",
+    "Monstein, 7278 Davos, Switzerland":"12",
+    "Glaris, 7277 Davos, Switzerland":"9",
+    "Frauenkirch, 7276 Davos, Switzerland":"8",
+    "Sertig-Dörfli, 7272 Davos, Switzerland":"10",
+    "Bahnhofstrasse 9, 7270 Davos Platz, Switzerland":"7",
+    "Praviganweg, 7270 Davos, Switzerland":"5",
+    "Berglistutz 3, 7270 Davos, Switzerland":"6",
+    "Tobelmühlestrasse 2, 7270 Davos, Switzerland":"14",
+    "Scalettastrasse 1, 7270 Davos Platz, Switzerland": "4",
+    "Aelastrasse 1, 7260 Davos, Switzerland":"3",
+    "Davos Dorf, Promenade 159, 7260 Davos Dorf, Switzerland":"1",
+    "Tschuggen, 7260 Davos, Switzerland":"2",
+    "Laret, 7265 Davos, Switzerland":"0"
 };
 class Path {
     constructor() {
@@ -53,31 +53,31 @@ async function fetchAsync () {
         let obj2 = json[i+1];
         let obj3 = json[i+2];
         let obj4 = json[i+3];
-        let origin = obj1["destination_addresses"].indexOf(obj1["origin_addresses"][0]);
+        let origin = lookup[obj1["origin_addresses"][0]];
 
         for(var x = 0; x < obj1["destination_addresses"].length; x++){
             let p = new Path;
             p.from = origin.toString();
-            p.to = x.toString();
+            p.to = lookup[obj1["destination_addresses"][x]];
             try{
                 p.carDuration = obj1["rows"][0]["elements"][x]["duration"]["value"].toString();
             }catch{
-                p.carDuration = "-1";
+                p.carDuration = "0";
             }
             try{
                 p.bicycleDuration = obj2["rows"][0]["elements"][x]["duration"]["value"].toString();
             }catch{
-                p.bicycleDuration = "-1";
+                p.bicycleDuration = "0";
             }
             try{
                 p.transitDuration = obj3["rows"][0]["elements"][x]["duration"]["value"].toString();
             }catch{
-                p.transitDuration = "-1";
+                p.transitDuration = "0";
             }
             try{
                 p.walkingDuration = obj4["rows"][0]["elements"][x]["duration"]["value"].toString();
             }catch{
-                p.walkingDuration = "-1";
+                p.walkingDuration = "0";
             }
             paths.push(p);
         }
